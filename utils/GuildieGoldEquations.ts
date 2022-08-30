cf18d262bd4c541e11c03b1f2de66c4957320b06
Dylan Kay <dylan@tech.melbourne>
2022-08-30 12:15:11 +0200

    Update equations

--- utils/GuildieGoldEquations.ts
+++ utils/GuildieGoldEquations.ts
@@ -1,19 +1,27 @@
 import { clamp, floor } from "lodash";
 
 export class GuildieGoldEquations {
+  static readonly STACKING_MULTIPLIER_RANGE = [3, 5];
+  static readonly RARITY_MULTIPLIER_RANGE = [2, 5];
+
   static stackingMultiplier(quantity: number) {
-    // y = 0.002154 * x^3 - 0.03578 * x^2 + 0.2098 * x + 1.558
     const x = clamp(quantity, 0, 10);
 
     if (x === 0) return 0;
 
+    // y = 0.002154 * x^3 - 0.03578 * x^2 + 0.2098 * x + 1.558
     const y =
       0.002154 * Math.pow(x, 3) -
       0.035578 * Math.pow(x, 2) +
       0.2098 * x +
       1.558;
 
-    return floor(y, 2);
+    const Y =
+      (this.STACKING_MULTIPLIER_RANGE[1] - this.STACKING_MULTIPLIER_RANGE[0]) *
+        y +
+      this.STACKING_MULTIPLIER_RANGE[0];
+
+    return floor(Y, 2);
   }
 
   static rarityMultiplier(rank: number) {
@@ -25,6 +33,10 @@ export class GuildieGoldEquations {
       0.0005698 * x +
       2.232;
 
-    return floor(y, 2);
+    const Y =
+      (this.RARITY_MULTIPLIER_RANGE[1] - this.RARITY_MULTIPLIER_RANGE[0]) * y +
+      this.RARITY_MULTIPLIER_RANGE[0];
+
+    return floor(Y, 2);
   }
 }
