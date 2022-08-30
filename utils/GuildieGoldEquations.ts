e9d2f7811d2d73375cb10ceeb659d9c25a64ff9e
Dylan Kay <dylan@tech.melbourne>
2022-08-30 14:56:29 +0200

    update equations

--- utils/GuildieGoldEquations.ts
+++ utils/GuildieGoldEquations.ts
@@ -1,41 +1,46 @@
 import { clamp, floor } from "lodash";
 
 export class GuildieGoldEquations {
-  static readonly STACKING_MULTIPLIER_RANGE = [3, 5];
-  static readonly RARITY_MULTIPLIER_RANGE = [2, 5];
+  static readonly STACKING_MULTIPLIER_RANGE = [Math.sqrt(2), Math.sqrt(4)];
+  static readonly RARITY_MULTIPLIER_RANGE = [Math.sqrt(2), Math.sqrt(4)];
 
   static stackingMultiplier(quantity: number) {
-    const x = clamp(quantity, 0, 10);
+    if (quantity <= 1) return 0;
 
-    if (x === 0) return 0;
+    const x = clamp(quantity, 1, 10);
 
-    // y = 0.002154 * x^3 - 0.03578 * x^2 + 0.2098 * x + 1.558
+    // y = 0.004274 * x^3 - 0.07099 * x^2 + 0.4162 * x - 0.3458
     const y =
-      0.002154 * Math.pow(x, 3) -
-      0.035578 * Math.pow(x, 2) +
-      0.2098 * x +
-      1.558;
+      0.004274 * Math.pow(x, 3) -
+      0.07099 * Math.pow(x, 2) +
+      0.4162 * x -
+      0.3458;
 
     const Y =
       (this.STACKING_MULTIPLIER_RANGE[1] - this.STACKING_MULTIPLIER_RANGE[0]) *
         y +
       this.STACKING_MULTIPLIER_RANGE[0];
 
+    // console.log({ Y, y, x, quantity });
+
     return floor(Y, 2);
   }
 
   static rarityMultiplier(rank: number) {
-    const x = clamp(rank, 0, 2222);
+    const x = clamp(rank, 1, 2222);
 
+    // y = -2.842e-10 * x^3 + 9.399e-7 * x^2 - 0.00113 * x + 0.9923
     const y =
-      -1.433e-10 * Math.pow(x, 3) +
-      4.74e-7 * Math.pow(x, 2) -
-      0.0005698 * x +
-      2.232;
+      -2.842e-10 * Math.pow(x, 3) +
+      9.399e-7 * Math.pow(x, 2) -
+      0.00113 * x +
+      0.9923;
 
     const Y =
       (this.RARITY_MULTIPLIER_RANGE[1] - this.RARITY_MULTIPLIER_RANGE[0]) * y +
       this.RARITY_MULTIPLIER_RANGE[0];
+
+    // console.log({ Y, y, x, rank });
 
     return floor(Y, 2);
   }
